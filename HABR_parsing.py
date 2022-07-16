dcedb1bf8fbca08415b648064e8dcc11128c4093
girnal82 <grinal82@gmail.com>
2022-07-16 21:29:11 +0300

    Added possibility to search through 10 pages

--- HABR_parsing.py
+++ HABR_parsing.py
@@ -11,22 +11,27 @@ HEADERS = {'Accept': '*/*', 'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5
 
 base_url = 'http://habr.com'
 url = base_url+'/ru/all/'
+#  adding the possiblility to search through 10 pages
+for i in range(1, 11):
+    page = 'page{}/'.format(i)
+    print(f'results from page {i}')
 
-response = requests.get(url, headers=HEADERS)
-text = response.text
+    response = requests.get(url+page, headers=HEADERS)
+    text = response.text
 
-soup = bs4.BeautifulSoup(text, features='html.parser')
-articles = soup.find_all('article')
-for article in articles:
-    date = article.find("time").attrs['title']
-    title = article.find("h2").find("span").text
-    href = article.find(class_="tm-article-snippet__title-link").attrs["href"]
-    news = article.find_all(
-        class_="tm-article-body tm-article-snippet__lead")
-    for i in news:
-        resulting_list = [j.text for j in i if j != ' ']
-        resulting_list = str(resulting_list)
-        for el in KEYWORDS:
-            if el in resulting_list:
-                print(
-                    f'ДАТА ПУБЛИКАЦИИ <{date}> / ЗАГОЛОВОК <{title}> / ССЫЛКА <{base_url+href}>\n' + "----"*20)
+    soup = bs4.BeautifulSoup(text, features='html.parser')
+    articles = soup.find_all('article')
+    for article in articles:
+        date = article.find("time").attrs['title']
+        title = article.find("h2").find("span").text
+        href = article.find(
+            class_="tm-article-snippet__title-link").attrs["href"]
+        news = article.find_all(
+            class_="tm-article-body tm-article-snippet__lead")
+        for i in news:
+            resulting_list = [j.text for j in i if j != ' ']
+            resulting_list = str(resulting_list)
+            for el in KEYWORDS:
+                if el in resulting_list:
+                    print(
+                        f'ДАТА ПУБЛИКАЦИИ <{date}> / ЗАГОЛОВОК <{title}> / ССЫЛКА <{base_url+href}>\n' + "----"*20)
